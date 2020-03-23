de4b18bb947cfa84661f69929df878844eb7da52
chamdav <chamdav@amazon.com>
2020-03-23 08:10:03 -0700

    implemented firebase, added ability to store users

--- crwn-clothing/src/firebase/firebase.utils.js
+++ crwn-clothing/src/firebase/firebase.utils.js
@@ -11,13 +11,39 @@ const config = {
   storageBucket: 'crwn-db-9af3d.appspot.com',
   messagingSenderId: '882035458041',
   appId: '1:882035458041:web:4362e8e0661abda72dc728',
-  measurementId: 'G-R1TWD90ME1'
+  measurementId: 'G-R1TWD90ME1',
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
 };
 
 firebase.initializeApp(config);
 
-export const auth = firebase.auth();
 export const firestore = firebase.firestore();
+export const auth = firebase.auth();
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({ prompt: 'select_account' });
